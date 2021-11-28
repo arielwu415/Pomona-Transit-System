6b8db4e8b2e7afa8059b9e0264cc9d6d97793544
arielwu415 <ariel070415@gmail.com>
2021-11-28 00:12:53 -0800

    Added jar files/Connected to MS Access

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String databaseURL = "jdbc:ucanaccess://Lab 4 Database1.accdb";
+		
+		try {
+			Connection connection = DriverManager.getConnection(databaseURL);
+			System.out.println("Connected to MS Access Database.");
+			
+			// SQL code goes here
+			String sql = "";
+			
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("Given database file does not exist.");
+		}
 	}
 
 }
