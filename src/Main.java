3fae7d2cf132bbedc58d26002945c9c91a6668f9
arielwu415 <ariel070415@gmail.com>
2021-11-28 19:10:59 -0800

    Added deleteTrip()/addTrips()/showTripOffering()

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 		Connection connection;
 		try {
 			connection = DriverManager.getConnection(databaseURL);
-			if(choice != 9) System.out.println("Connected to MS Access Database.\n");
+			if(choice != 9) System.out.println("Connected to MS Access Database.");
 				
 			// Perform action according to user input
 			optionSwitch(choice, connection);
@@ -62,6 +62,12 @@ public class Main {
 
 	public static void optionSwitch(int choice, Connection connection) throws SQLException {
 		
+		Scanner scan = new Scanner(System.in);
+		while (choice > 9 || choice <= 0 ) {
+			System.out.println("Invalid input. Please try again:");
+			choice = scan.nextInt();
+		}
+		
 		while(choice < 9 && choice > 0) {
 			
 			switch(choice) {
@@ -69,19 +75,20 @@ public class Main {
 					showTripInfo(connection);
 					break;
 				case 2:
-					
+					editTripOffering(connection);
+					break;
 				case 3:
-					
+					break;
 				case 4:
-					
+					break;
 				case 5:
-					
+					break;
 				case 6: 
-					
+					break;
 				case 7:
-					
+					break;
 				case 8:
-					
+					break;
 				case 9:
 					return;
 				default:
@@ -97,10 +104,10 @@ public class Main {
 	
 	
 	@SuppressWarnings("resource")
-	public static void showTripInfo(Connection connection) throws SQLException {
+	public static void showTripInfo(Connection connection) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.print("> Please enter the start location: ");
+		System.out.print("\n> Please enter the start location: ");
 		String startLocation = scan.nextLine();
 		System.out.print("> Please enter the destination: ");
 		String destination = scan.nextLine();
@@ -116,19 +123,159 @@ public class Main {
 				+ "AND Tripdate = \"" + date + "\"";
 		
 		
-		Statement statement = connection.createStatement();
-		ResultSet result = statement.executeQuery(sql);
-		System.out.println("\n--------------- Schedule on " + date + " ---------------");
-		System.out.println(String.format("%-15s %-15s %-15s %-10s", "Start Time", "Arrival Time", "Driver Name", "Bus ID"));
+		Statement statement;
+		try {
+			statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			System.out.println("\n--------------- Schedule on " + date + " ---------------");
+			System.out.println(String.format("%-15s %-15s %-15s %-10s", "Start Time", "Arrival Time", "Driver Name", "Bus ID"));
 		
-		while(result.next()) {
-			String startTime = result.getString("ScheduledStartTime");
-			String arrivalTime = result.getString("ScheduledArrivalTime");
-			String driver = result.getString("DriverName");
-			int busID = result.getInt("BusID");
-			
-			System.out.println(String.format("%-15s %-15s %-15s %-10s",
-							startTime, arrivalTime, driver, busID));
+			while(result.next()) {
+				String startTime = result.getString("ScheduledStartTime");
+				String arrivalTime = result.getString("ScheduledArrivalTime");
+				String driver = result.getString("DriverName");
+				int busID = result.getInt("BusID");
+				
+				System.out.println(String.format("%-15s %-15s %-15s %-10s",
+								startTime, arrivalTime, driver, busID));
+			}
+		
+			statement.close();
+		} catch (SQLException e) {
+			System.out.println("Schedule searching error.");
 		}
+		
+	}
+	
+	
+	@SuppressWarnings("resource")
+	public static void editTripOffering(Connection connection) throws SQLException {
+		
+		Scanner scan = new Scanner(System.in);
+		boolean keepEditing = true;
+		
+		showTripOffering(connection);
+		
+		while (keepEditing) {
+			System.out.print("\nNow do you want to ...?\n"
+					+ "\t(1) Delete a trip offering\n"
+					+ "\t(2) Add trips\n"
+					+ "\t(3) Change driver for a trip\n"
+					+ "\t(4) Change bus for a trip\n"
+					+ "\t(5) Show all trips offering\n"
+					+ "\t(6) Go back to main menu\n> ");
+			int choice = scan.nextInt();
+			switch(choice) {
+				case 1:
+					deleteTrip(connection);
+					break;
+				case 2:
+					addTrips(connection);
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					showTripOffering(connection);
+					break;
+				case 6:
+					return;
+				default:
+					System.out.println("Invalid input. Please try again.");
+			}			
+		}
+		
+	}
+	
+	public static void showTripOffering(Connection connection) {
+		
+		String sql = "SELECT * FROM TripOffering";
+		Statement statement;
+		try {
+			statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			
+			System.out.println("\n----------------------------------- Trip Offering -----------------------------------");
+			System.out.println(String.format("%-15s %-15s %-15s %-15s %-15s %-15s", "Trip Number", "Date", "Start Time", "Arrival Time", "Driver Name", "Bus ID"));
+			while(result.next()) {
+				String tripNum = result.getString("TripNumber");
+				String date = result.getString("TripDate");
+				String startTime = result.getString("ScheduledStartTime");
+				String arrivalTime = result.getString("ScheduledArrivalTime");
+				String driver = result.getString("DriverName");
+				int bus = result.getInt("BusID");
+				
+				
+				System.out.println(String.format("%-15s %-15s %-15s %-15s %-15s %-15s", tripNum, date, startTime, arrivalTime, driver, bus));
+				
+			}
+		} catch (SQLException e){
+			
+		}
+	}
+	
+	@SuppressWarnings("resource")
+	public static void deleteTrip(Connection connection) {
+				
+		Scanner scan = new Scanner(System.in);
+		System.out.print("\n> Please enter the trip number you want to delete: ");
+		String tripNum = scan.nextLine();
+		System.out.print("> Please enter the date: ");
+		String date = scan.nextLine();
+		System.out.print("> Please enter the start time: ");
+		String startTime = scan.nextLine();
+		
+		String sql_delete = "DELETE * FROM TripOffering "
+				+ "WHERE TripNumber = \"" + tripNum + "\" "
+				+ "AND TripDate = \"" + date + "\" "
+				+ "AND ScheduledStartTime = \"" + startTime + "\" ";
+		
+		Statement statement;
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql_delete);
+			statement.execute(sql_delete);
+		} catch (SQLException e) {
+			System.out.println("Record deletion unsuccessful.");
+		}
+		
+		System.out.println("Record deleted.");
+	}
+	
+	
+	@SuppressWarnings("resource")
+	public static void addTrips(Connection connection) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("> Please enter the trip number you want to add: ");
+		String tripNum = scan.nextLine();
+		System.out.print("> Please enter the date: ");
+		String date = scan.nextLine();
+		System.out.print("> Please enter the start time: ");
+		String startTime = scan.nextLine();
+		System.out.print("> Please enter the arrival time: ");
+		String arrivalTime = scan.nextLine();
+		System.out.print("> Please enter the driver name: ");
+		String driver = scan.nextLine();
+		System.out.print("> Please enter the bus ID: ");
+		int bus = scan.nextInt();
+		
+		String sql = "INSERT INTO TripOffering VALUES (\"" + tripNum + "\", "
+				+ "\"" + date + "\", "
+				+ "\"" + startTime + "\", "
+				+ "\"" + arrivalTime + "\", "
+				+ "\"" + driver + "\", "
+				+ bus + ")";
+		
+		Statement statement;
+		try {
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			statement.execute(sql);
+		} catch (SQLException e) {
+			// Exception is thrown even when data are inserted successfully
+		}
+		
 	}
 }
